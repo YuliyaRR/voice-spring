6ae8d5a6203202b02c095263a6c8ceb002ea83c7
Yuliya Romashko <ramashkay@gmail.com>
2023-04-28 23:32:42 +0300

    Updated: GlobalExceptionHandler

--- src/main/java/groupwork/web/controllers/GlobalExceptionHandler.java
+++ src/main/java/groupwork/web/controllers/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
-package groupwork.exception;
+package groupwork.web.controllers;
 
 import groupwork.dto.ResponseError;
+import groupwork.exception.ConnectionDataBaseException;
+import groupwork.exception.InvalidInputServiceException;
+import groupwork.exception.NotFoundDataBaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,9 +22,9 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ConnectionDataBaseException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)//503?
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseError handle(ConnectionDataBaseException e){
-        return new ResponseError((e.getMessage()) + ": " + e.getCause(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseError((e.getMessage()) + ": " + e.getCause().getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(InvalidInputServiceException.class)
@@ -29,9 +32,9 @@ public class GlobalExceptionHandler {
     public ResponseError handle(InvalidInputServiceException e) {
         return new ResponseError(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseError handle(RuntimeException e){
+    public ResponseError handle(Exception e){
         return new ResponseError(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
